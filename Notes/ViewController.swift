e9f258511560b9a5a81f63a9daba4e440825f64b
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2021-07-08 18:26:14 +0530

    Add sounds, Fix AutoLayout for iPhone 4s

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var player: AVAudioPlayer!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        // print(sender.currentTitle!)
+        sender.alpha = 0.5
+        sleep(UInt32(0.3))
+        sender.alpha = 1
+        playFile(fileName: sender.currentTitle!)
     }
-
-
+    
+    func playFile(fileName: String) {
+        // print(fileName)
+        let url = Bundle.main.url(forResource: fileName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
